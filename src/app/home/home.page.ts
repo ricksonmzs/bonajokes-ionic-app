f171cdd56701c7b288bc2975a62ebaee3addd2e2
Hélio Cardoso Rodrigues de Souza <heliosouza@gmail.com>
2019-07-25 15:45:16 -0300

    BonaJokes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { JokesService } from './../services/jokes.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private jokesService: JokesService) {}
 
+  piada: any;
+  piadas: any[];
+  valorInput: string;
+  url = 'https://yt3.ggpht.com/a-/AAuE7mB26ZPmUSpTqFfDH-0TtH7C1ezO3W28JPyOIw=s288-mo-c-c0xffffffff-rj-k-no';
+  piadasFilter: any[];
+
+  // ngOnInit(){
+  //   this.getJokes();
+  // }
+
+  showJokes(){
+    this.jokesService.getAllJokes().subscribe(piadas =>{
+      this.piada = piadas[this.indexAleatorio(piadas)];
+    }, erro => {
+      console.log(erro);
+    }, () => {
+      console.log('sucesso');
+    });
+  }
+
+  indexAleatorio(piadas){
+    const index = Math.floor(Math.random() * piadas.length+1);
+    return index;
+  }
+
+  getJokes(){
+    this.jokesService.getAllJokes().subscribe(dados =>{
+      this.piadas = dados;
+      this.piadasFilter = this.piadas.filter ( dado => {
+        if(dado.pergunta.toUpperCase().match(this.valorInput.toUpperCase())) {
+          return dado;
+        }
+      })
+    }, erro => {
+      console.log(erro);
+    }, () => {
+      console.log('sucesso');
+    });
+  }
+
+  searchOnJokes(){
+    this.piadasFilter = this.piadas.filter ( dado => {
+      if(dado.pergunta.match(this.valorInput)) {
+        return dado;
+      }
+    })
+  }
 }
---
+++ src/app/services/jokes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokesService {
+
+  url = '/assets/jokes.json';
+
+  constructor(private http: HttpClient) { }
+
+  getAllJokes(){
+    return this.http.get<any[]>(this.url);
+  }
+}
